e7e6ba14f3c2fb87fecb00da2da70a91347a4782
Lauren <laurchia@gmail.com>
2018-03-29 12:23:50 +1100

    Add switching between players

--- src/index.js
+++ src/index.js
@@ -18,14 +18,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
     {/* Array of 9 nulls that correspond to the 9 squares */}    
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -38,7 +42,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
